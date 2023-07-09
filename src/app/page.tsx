17732005b5daaae5756ba2921fd7c77d787c0928
shinbom <tlsqha@gmail.com>
2023-07-09 11:15:11 +0900

    fixed : build error fix
    - metaData 'use client' 사용으로 오류 나는 문제 해결
    - portal 사용을 위한 recoil 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-'use client'
-
 import { Metadata } from 'next'
-import { useState } from 'react'
 
 import Header from '@/components/Header'
 import MainContainer from '@/components/Main/MainContainer'
-import Modal from '@/modules/Modal/components/Modal'
-import MemeItem from '@/modules/Meme/components/MemeItem/MemeItem'
 
 export const metadata: Metadata = {
   title: '짤리우드',
@@ -14,22 +9,11 @@ export const metadata: Metadata = {
 }
 
 export default async function Home() {
-  const [modalStatus, setModalStatus] = useState(false)
 
   return (
     <div>
       <Header />
       <MainContainer />
-      {
-        modalStatus ? <Modal
-        title="테스트"
-      >
-        <MemeItem 
-          id={1}
-          filePath='test'
-        />
-      </Modal> : null
-      }
     </div>
   )
 }
