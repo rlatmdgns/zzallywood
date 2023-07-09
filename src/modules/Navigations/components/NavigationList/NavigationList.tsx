02770fe1fd36c177249d258e59509f06ea7c1375
shinbom <tlsqha@gmail.com>
2023-07-09 11:02:36 +0900

    refactor: 네비게이션 타입 추가
    - 업로드 버튼 생성

--- src/modules/Navigations/components/NavigationList/NavigationList.tsx
+++ src/modules/Navigations/components/NavigationList/NavigationList.tsx
@@ -7,6 +7,7 @@ import NavigationItem from '../NavigationItem/NavigationItem'
 interface NavigationItemProps {
   title: string
   path: string
+  type: string
 }
 
 interface navigationProps {
@@ -18,6 +19,7 @@ const NavigationList = ({ navigations }: navigationProps) => {
     <NavigationItem
       title={navItem.title}
       path={navItem.path}
+      type={navItem.type}
       key={navItem.title}
     />
   ))
