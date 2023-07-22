9a89b3e5b122c9a0b1f924c1353cdbfc8fe6acbd
김승훈 <rlatmdgns94@naver.com>
2023-07-22 16:31:16 +0900

    refactor: api 변경으로 인한 네이밍 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,18 @@ import { Metadata } from 'next'
 
 import Header from '@/components/Header'
 import NavigationModule from '@/components/Navigations'
-import HomeModule from '@/modules/components'
+import TeamModule from '@/modules/Team'
 
 export const metadata: Metadata = {
   title: '짤리우드',
   description: '아이돌 짤 저장소',
 }
 
-export default async function Home() {
-
+export default function Home() {
   return (
     <div>
       <Header />
-      <HomeModule />
+      <TeamModule />
       <NavigationModule />
     </div>
   )
--- src/modules/components/Idol/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.section`
-  padding: 10px;
-`
-export const Name = styled.h3`
-  font-size: 16px;
-`
