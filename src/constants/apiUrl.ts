c3e24ee4551af106d9993a09bca47c8c24354def
김승훈 <rlatmdgns94@naver.com>
2023-07-25 01:32:13 +0900

    feat: 타입 지정

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,6 +1,6 @@
 export const API_URL = {
   TEAMS: '/teams',
   MEMES: '/memes',
-  TEAM_MEMBERS: (id) => `/teams/${id}/members`,
-  MEMBER_MEMES: (id) => `/members/${id}/memes`,
+  TEAM_MEMBERS: (id: string) => `/teams/${id}/members`,
+  MEMBER_MEMES: (id: string) => `/members/${id}/memes`,
 }
