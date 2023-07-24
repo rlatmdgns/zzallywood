0a277f3ead1e8fb3e022dfe75b18f161f6784b06
김승훈 <rlatmdgns94@naver.com>
2023-07-24 21:42:00 +0900

    styles: 오타 수정

--- src/common/components/Navigations/components/NavigationItem/styles.ts
+++ src/common/components/Navigations/components/NavigationItem/styles.ts
@@ -1,26 +1,26 @@
 import styled from 'styled-components'
 
 export const NavigationItem = styled.li`
-flex-grow:1;
+  flex-grow: 1;
 `
 export const NavigationText = styled.span`
-  display:block;
-  width:100%;
-  text-align:center;
+  display: block;
+  width: 100%;
+  text-align: center;
 `
 
 export const UploadButtonWrap = styled.div`
-  position:relative;
+  position: relative;
 `
 
 export const UploadInput = styled.input`
-  position:absolute;
-  top:0;
-  left:0;
-  width:1px;
-  hieght:1px;
-  overflow:hidden;
-  clip:rect(0, 0, 0, 0);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
 `
 
 export const UploadButton = styled.label`
