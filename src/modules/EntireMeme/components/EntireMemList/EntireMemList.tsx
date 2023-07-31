2839acb8804b517a8794da2183429ef7e4e2e045
김승훈 <rlatmdgns94@naver.com>
2023-07-31 22:30:00 +0900

    feat: scroll 적용

--- src/modules/EntireMeme/components/EntireMemList/EntireMemList.tsx
+++ src/modules/EntireMeme/components/EntireMemList/EntireMemList.tsx
@@ -1,20 +1,13 @@
-'use client'
-
 import React from 'react'
 import CardList from '@/common/components/CardList'
 import { Meme } from '@/services/getMemes/getMemes.interface'
-import * as Styles from './styles'
 
 interface EntireMemListProps {
   memes: Meme[]
 }
 
 const EntireMemList = ({ memes }: EntireMemListProps) => {
-  return (
-    <Styles.Wrapper>
-      <CardList memes={memes} />
-    </Styles.Wrapper>
-  )
+  return <CardList memes={memes} />
 }
 
 export default EntireMemList
