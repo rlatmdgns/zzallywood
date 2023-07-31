0abed8622f3395e2402259930155fe45ac2cf6a2
김승훈 <rlatmdgns94@naver.com>
2023-07-31 21:12:37 +0900

    test: 멤버 요청

--- src/modules/MemberTab/MemberTab.tsx
+++ src/modules/MemberTab/MemberTab.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import getMembers from '@/services/getMembers'
 
 interface MemberTabProps {
   id: string
 }
 
 const MemberTab = async ({ id }: MemberTabProps) => {
-  const { content } = await getMembers(id)
-  console.log(content)
   return <div></div>
 }
 
