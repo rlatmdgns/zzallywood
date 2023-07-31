c88bd57c387f5881c4fb060c20c9ed0f5140b0df
김승훈 <rlatmdgns94@naver.com>
2023-07-31 22:24:50 +0900

    feat: 메인페이지 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from 'next'
 import React, { Suspense } from 'react'
 import TeamModule from '@/modules/Team'
+import Header from '@/common/components/Header'
+import EntireMeme from '@/modules/EntireMeme'
+import NavigationModule from '@/common/components/Navigations'
 
 export const metadata: Metadata = {
   title: '짤리우드',
@@ -10,12 +13,14 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div>
-      {/*<Header />*/}
+      <Header />
       <Suspense fallback={<></>}>
         <TeamModule />
       </Suspense>
-      {/*<EntireMeme />*/}
-      {/*<NavigationModule />*/}
+      <Suspense fallback={<></>}>
+        <EntireMeme />
+      </Suspense>
+      <NavigationModule />
     </div>
   )
 }
