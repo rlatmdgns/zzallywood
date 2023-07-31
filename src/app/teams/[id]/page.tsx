6368e0276fa7bcc22a60cd0f24f20e87bb4a3bbb
김승훈 <rlatmdgns94@naver.com>
2023-07-31 21:18:20 +0900

    test: 멤버 요청

--- src/app/teams/[id]/page.tsx
+++ src/app/teams/[id]/page.tsx
@@ -3,14 +3,11 @@ import React from 'react'
 import Header from '@/common/components/Header'
 import { useParams, useSearchParams } from 'next/navigation'
 import MemberTab from '@/modules/MemberTab'
-import getMembers from '@/services/getMembers'
 
 const TeamPage = async () => {
   const { id } = useParams()
   const searchParams = useSearchParams()
   const memberId = searchParams.get('memberId')
-  const { content } = await getMembers(id)
-  console.log(content)
   return (
     <div>
       <Header />
--- src/modules/MemberTab/MemberTab.tsx
+++ src/modules/MemberTab/MemberTab.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { use } from 'react'
+import getMembers from '@/services/getMembers'
 
 interface MemberTabProps {
   id: string
 }
 
 const MemberTab = async ({ id }: MemberTabProps) => {
+  const { content } = use(getMembers(id))
+  console.log(use)
   return <div></div>
 }
 
